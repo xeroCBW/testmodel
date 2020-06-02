cf859403fff3c6745a34407ced829291fe697e67
chenBW <xero2022@163.com>
2020-06-02 15:44:17 +0800

    完成filter设置

--- mall/index/views.py
+++ mall/index/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def indexView(request):
 
-    # 设置基本数据
-    return render(request,'index.html',locals())
+    type_list = Product.objects.values('type').distinct()
+    name_list = Product.objects.values('name','type')
+    # 设置首页信息
+    context = {'title': '首页', 'type_list': type_list, 'name_list': name_list}
+    return render(request, 'index.html',context=context, status=200)
