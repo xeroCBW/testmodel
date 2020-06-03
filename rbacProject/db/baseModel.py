9487fd9648dabbc168ad7964667fb25ac080d60e
chenBW <xero2022@163.com>
2020-06-03 15:37:19 +0800

    完成环境搭建

---
+++ rbacProject/db/baseModel.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class BaseModel(models.Model):
+
+    create_user = models.CharField('创建用户',max_length=50)
+    create_time = models.DateTimeField('创建时间', default=timezone.now)
+    update_user = models.CharField('更新用户',max_length=50)
+    update_time = models.DateTimeField('更新时间',default=timezone.now)
+
+    note = models.CharField('备注',max_length=200)
+    status = models.IntegerField('状态')
+
+    class Meta:
+        abstract = True
---
+++ rbacProject/index/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def indexView(request):
+
+
+    return render(request,'index.html')
