d062707ece7cdd6bd9ca949e2d07e34f670fae6a
chenBW <xero2022@163.com>
2020-06-07 18:40:36 +0800

    初步完成缓存

--- mall/index/views.py
+++ mall/index/views.py
@@ -1,24 +1,57 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
 from index.forms import ProductModelForm
 from .models import *
 from django.contrib.auth import login,logout,authenticate
-# Create your views here.
+
+
+
 def indexView(request):
 
     type_list = Type.objects.values('type_name').distinct()
     name_list = Product.objects.values('name','type__type_name')
-    # 设置首页信息
-    context = {'title': '首页', 'type_list': type_list, 'name_list': name_list}
-    return render(request, 'index.html',context=context, status=200)
+    title = '首页'
 
 
+    product = request.GET.get('product', '')
+    price = request.GET.get('price', '')
+    if product:
+        # 获取存储在Session的数据，如果Session不存在product_info，则返回一个空列表
+        product_list = request.session.get('product_info', [])
+        # 判断当前请求参数是否已存储在Session
+        if not product in product_list:
+            # 将当前参数存储在列表product_list
+            product_list.append({'price': price, 'product': product})
+        # 更新存储在Session的数据
+        request.session['product_info'] = product_list
+        return redirect('/')
+
+    return render(request, 'index.html',locals())
+
+
+@login_required(login_url = '/user/login')
 def shoppingCarView(request):
+    # 获取存储在Session的数据，如果Session不存在product_info，则返回一个空列表
+    product_list = request.session.get('product_info', [])
+    # 获取GET请求参数，如果没有请求参数，返回空值
+    del_product = request.GET.get('product', '')
+    # 判断是否为空，若非空，删除Session里的商品信息
 
-    # 返回购物车
 
-    return render(request,'shopoingCar.html',locals())
+    print(product_list)
+    print(del_product)
+
+    if del_product:
+        # 删除Session里某个商品数据
+        for i in product_list:
+            if i['product'] == del_product:
+                product_list.remove(i)
+        # 将删除后的数据覆盖原来的Session
+        request.session['product_info'] = product_list
+        return redirect('/shoppingCar')
+    return render(request, 'shoppingCar.html', locals())
 
 
 def productAddView(request):
