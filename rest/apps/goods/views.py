40aa5b667fd49355ed03890fd8647b8ddcbd1b43
chenBW <xero2022@163.com>
2020-06-16 16:43:02 +0800

    完成搜索排序等功能

--- rest/apps/goods/views.py
+++ rest/apps/goods/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
-from rest_framework import generics, mixins, viewsets
-from rest_framework.pagination import PageNumberPagination
+# googd/views.py
+
 from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from goods.filters import GoodsFilter
+from goods.serializers import GoodsSerializer
 from .models import Goods
-from .serializers import GoodsSerializer
+from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets
+from .filters import GoodsFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-# Create your views here.
 
 class GoodsPagination(PageNumberPagination):
     '''
     商品列表自定义分页
     '''
     #默认每页显示的个数
-    page_size = 12
+    page_size = 10
     #可以动态改变每页显示的个数
     page_size_query_param = 'page_size'
     #页码参数
@@ -29,11 +31,17 @@ class GoodsListViewSet(mixins.ListModelMixin,viewsets.GenericViewSet):
     '商品列表页'
 
     #这里必须要定义一个默认的排序,否则会报错
-    queryset = Goods.objects.all().order_by('id')
+    queryset = Goods.objects.all()
     # 分页
     pagination_class = GoodsPagination
+    #序列化
     serializer_class = GoodsSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter)
 
     # 设置filter的类为我们自定义的类
+    #过滤
     filter_class = GoodsFilter
+    #搜索,=name表示精确搜索，也可以使用各种正则表达式
+    search_fields = ('=name','goods_brief')
+    #排序
+    ordering_fields = ('sold_num', 'add_time')
