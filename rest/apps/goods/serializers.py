af127b6600553268f54ae241c6cd830d20890175
chenBW <xero2022@163.com>
2020-06-16 16:17:12 +0800

    实现所有字段的序列化

--- rest/apps/goods/serializers.py
+++ rest/apps/goods/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(default="",max_length=20)
-    click_num = serializers.IntegerField(default=0,)
-    goods_front_image = serializers.ImageField()
+# class GoodsSerializer(serializers.Serializer):
+#     name = serializers.CharField(default="",max_length=20)
+#     click_num = serializers.IntegerField(default=0,)
+#     goods_front_image = serializers.ImageField()
+from goods.models import Goods
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goods
+        fields = '__all__'
