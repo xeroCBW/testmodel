a84a7f15f25f6c3b2e52c2e715ce49824294698b
chenBW <861754186@qq.com>
2020-07-26 11:25:28 +0800

    完成项目的回调

--- RestAdvanceAdm/apps/system/tasks.py
+++ RestAdvanceAdm/apps/system/tasks.py
@@ -8,18 +8,24 @@ class demotask(Task):
 
     def on_success(self, retval, task_id, args, kwargs):   # 任务成功执行
 
+        print('on_success')
+
         logger.info('task id:{} , arg:{} , successful !'.format(task_id,args))
 
 
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):  #任务失败执行
+
+        print('on_failure')
         logger.info('task id:{} , arg:{} , failed ! erros : {}' .format(task_id,args,exc))
 
 
     def on_retry(self, exc, task_id, args, kwargs, einfo):    #任务重试执行
+
+        print('on_retry')
         logger.info('task id:{} , arg:{} , retry !  einfo: {}'.format(task_id, args, exc))
 
-@shared_task
+@task(base=demotask)
 def add(x, y):
 
     return x + y
