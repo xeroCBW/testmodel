a69585a49a8ee1a5aebbfb0ef0f5832efcec3fa9
chenBW <861754186@qq.com>
2020-07-26 10:38:08 +0800

    实现简单的render

--- PythonBase/14tornado实现简单的api.py
+++ PythonBase/14tornado实现简单的api.py
@@ -7,8 +7,14 @@ import tornado.ioloop
 class basicRequestHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write('hello')
+        self.render('index.html')
 
+
+class queryStringRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        n = int(self.get_argument('n'))
+        res = 'odd' if n % 2 else 'even'
+        self.write('this number %d is %s' %(n,res))
 
 
 
@@ -21,7 +27,7 @@ if __name__ == '__main__':
         [
             (r'/',basicRequestHandler),
             (r'/blog', basicRequestHandler),
-            (r'/isEven', basicRequestHandler),
+            (r'/isEven', queryStringRequestHandler),
             (r'/tweet/([0-9]+)', basicRequestHandler),
 
         ]
