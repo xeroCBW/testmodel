87c123b2296a8a9937599853d061c37b3c3f9cf4
chenBW <861754186@qq.com>
2020-08-05 21:50:01 +0800

    实现失效用户无法登陆

--- RestAdm/RestAdm/settings.py
+++ RestAdm/RestAdm/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'utils.check_token_middleware.CheckTokenMiddleware',
+    'utils.check_token_middleware.CheckTokenMiddleware',
     'utils.log_middleware.RequestLogMiddleware',
     # 'utils.disable_csrf_middleware.DisableCSRFCheck',
     # 'utils.ip_block_middleware.IPBlockMiddleWare',
--- RestAdm/utils/check_token_middleware.py
+++ RestAdm/utils/check_token_middleware.py
@@ -20,38 +20,48 @@ class CheckTokenMiddleware(MiddlewareMixin):
 
     def process_request(self,request):
 
-        jwt_token = request.headers.get('Authorization', None)
-        if jwt_token is not None and jwt_token != '':
-            data = {
-                'token': jwt_token.split()[1],  # [0] 是前缀，默认为JWT
-            }
+        if request and request.user:
+            user = getattr(request,'user')
+            if len(user.username) and hasattr(user,'is_active') and not getattr(user,'is_active'):
+                print('-----')
+                return JsonResponse(data={'msg': '用户不可用请联系超级管理员', 'data': {}, 'code': 401},
+                                    status=status.HTTP_401_UNAUTHORIZED)
 
-            try:
-                valid_data = VerifyJSONWebTokenSerializer().validate(data)
-                user = valid_data['user']
-            except (InvalidSignatureError, ValidationError):
-                # 找不到用户，说明token 不合法或者身份过期
-                return JsonResponse(data={'msg': '身份已经过期或不合法，请重新登入','data':data,'code':401}, status=status.HTTP_401_UNAUTHORIZED)
 
-            if user.user_secret != data['token']:
-                user.user_secret = uuid4()
-                user.save()
-                return JsonResponse(data={'msg': '登录状态异常,请重新登入', 'data': data, 'code': 401},status=status.HTTP_401_UNAUTHORIZED)
+            # data = {
+            #     'token': jwt_token.split()[1],  # [0] 是前缀，默认为JWT
+            # }
+            #
+            # try:
+            #     valid_data = VerifyJSONWebTokenSerializer().validate(data)
+            #     user = valid_data['user']
+            # except (InvalidSignatureError, ValidationError):
+            #     # 找不到用户，说明token 不合法或者身份过期
+            #     return JsonResponse(data={'msg': '身份已经过期或不合法，请重新登入','data':data,'code':401}, status=status.HTTP_401_UNAUTHORIZED)
+            #
+            # if user.user_secret != data['token']:
+            #     user.user_secret = uuid4()
+            #     user.save()
+            #     return JsonResponse(data={'msg': '登录状态异常,请重新登入', 'data': data, 'code': 401},status=status.HTTP_401_UNAUTHORIZED)
 
-    def process_response(self, request, response):
-        # 仅用于处理 login请求
-        if request.path == '/user/login' and request.method == 'POST' and response.data.get('token'):
-            data = response.data
-            try:
-                valid_data = VerifyJSONWebTokenSerializer().validate(data)
-                user = valid_data['user']
-                user.user_secret = valid_data['token']
-                user.save()
-            except(InvalidSignatureError, ValidationError):
-                # 找不到用户，说明token 不合法或者身份过期
-                return JsonResponse(data={'msg': '用户名或者密码错误,请重新登录','data':data,'code':400}, status=status.HTTP_400_BAD_REQUEST)
-
-            return response
-        else:
-            return response
+    # def process_response(self, request, response):
+    #     # 仅用于处理 login请求
+    #
+    #     print('-----')
+    #
+    #     pass
+    #     # if request.path == '/user/login' and request.method == 'POST' and response.data.get('token'):
+    #     #     data = response.data
+    #     #     try:
+    #     #         valid_data = VerifyJSONWebTokenSerializer().validate(data)
+    #     #         user = valid_data['user']
+    #     #         user.user_secret = valid_data['token']
+    #     #         user.save()
+    #     #     except(InvalidSignatureError, ValidationError):
+    #     #         # 找不到用户，说明token 不合法或者身份过期
+    #     #         return JsonResponse(data={'msg': '用户名或者密码错误,请重新登录','data':data,'code':400}, status=status.HTTP_400_BAD_REQUEST)
+    #     #
+    #     #     return response
+    #     # else:
+        #     return response
 
